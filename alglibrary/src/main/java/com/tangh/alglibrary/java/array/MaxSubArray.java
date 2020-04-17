74bae5ef8e28bd3fb3b1607302fa0132a89cc562
汤浩 <tanghao@hupu.com>
2020-04-17 21:51:23 +0800

    first commit

--- alglibrary/src/main/java/com/tangh/alglibrary/java/array/MaxSubArray.java
+++ alglibrary/src/main/java/com/tangh/alglibrary/java/array/MaxSubArray.java
@@ -20,13 +20,16 @@ public class MaxSubArray {
     }
 
     public static int maxSubArray(int[] nums) {
-        int n = nums.length;
-        int currSum = nums[0], maxSum = nums[0];
 
-        for(int i = 1; i < n; ++i) {
-            currSum = Math.max(nums[i], currSum + nums[i]);
-            maxSum = Math.max(maxSum, currSum);
+
+        int currNums = nums[0];
+        int maxNums = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            currNums = Math.max(currNums + nums[i], nums[i]);
+            maxNums=Math.max(maxNums,currNums);
         }
-        return maxSum;
+
+        return maxNums;
+
     }
 }
