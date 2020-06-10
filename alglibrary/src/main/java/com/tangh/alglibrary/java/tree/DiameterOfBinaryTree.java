719d3f440f19a575139afc2e017e2f2c55f83a1d
汤浩 <tanghao@hupu.com>
2020-06-10 16:11:13 +0800

    first commit

---
+++ alglibrary/src/main/java/com/tangh/alglibrary/java/tree/DiameterOfBinaryTree.java
@@ -0,0 +1,35 @@
+package com.tangh.alglibrary.java.tree;
+
+/**
+ * 543. 二叉树的直径
+ * <p>
+ * 给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
+ * <p>
+ * 示例 :
+ * 给定二叉树
+ * <p>
+ * 1
+ * / \
+ * 2   3
+ * / \
+ * 4   5
+ * 返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+ * <p>
+ * 注意：两结点之间的路径长度是以它们之间边的数目表示。
+ */
+public class DiameterOfBinaryTree {
+    public static void main(String[] args) {
+
+    }
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root != null) {
+            diameterOfBinaryTree(root.left);
+            int val=root.val;
+            diameterOfBinaryTree(root.right);
+        }
+
+        return -1;
+    }
+
+}
